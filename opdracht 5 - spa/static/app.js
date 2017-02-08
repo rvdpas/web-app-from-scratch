bad29a7e8024a24d03c31cdb5885c8388dd9cad8
Oliver <hamburger602@gmail.com>
2017-02-08 21:28:02 +0100

    added opdracht 5 folders

---
+++ opdracht 5 - spa/static/app.js
@@ -0,0 +1,22 @@
+(function() {
+    "use strict";
+
+    var app = {
+        init: function() {
+            routes.init();
+            console.log(location.hash);
+        }
+    };
+
+    var routes = {
+
+    };
+
+    var sections = {
+
+    };
+
+    app.init();
+
+    }()
+);
