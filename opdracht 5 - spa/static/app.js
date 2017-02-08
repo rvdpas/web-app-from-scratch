5324fdcf49db5be321d0d2d55966cf9c4aec3b71
Oliver <hamburger602@gmail.com>
2017-02-08 22:56:34 +0100

    added JS, changed HTML, and CSS

--- opdracht 5 - spa/static/app.js
+++ opdracht 5 - spa/static/app.js
@@ -5,15 +5,35 @@
         init: function() {
             routes.init();
             console.log(location.hash);
+            console.log(Date())
         }
+
     };
 
     var routes = {
+        init: function() {
+            // var oldLocation = location.hash;
 
+            window.addEventListener('hashchange', function(){
+                sections.toggle(location.hash);
+            })
+
+        }
     };
 
     var sections = {
+        toggle: function(route) {
+            var mySection = document.querySelectorAll('section');
 
+            mySection.forEach( function(e) {
+                if('#'+e.id === route) {
+                    e.classList.remove('section__hide');
+                } else {
+                    e.classList.add('section__hide');
+                }
+            });
+
+        }
     };
 
     app.init();
